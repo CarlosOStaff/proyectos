88faf68b26e0c2ae8a4e11ecd3d73fd6f0f17307
CarlosOStaff <carlos.ovando@staffbridge.com.mx>
2024-09-12 12:00:09 -0600

    actualizacon de css de login y forgotpassword

--- ProyectoPruebaPhp/Vistas/Auth/forgotpassword.php
+++ ProyectoPruebaPhp/Vistas/Auth/forgotpassword.php
@@ -10,27 +10,28 @@ session_start();
     <title>Recuperar Contraseña - Librería</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../Css/auth/forgotpassword.css">
     <link rel="stylesheet" href="../../Css/modal.css">
 </head>
 
 <body>
     <div class="container">
         <div class="row justify-content-center align-items-center vh-100">
-            <div class="col-md-6 col-lg-4">
-                <div class="card shadow-lg p-4 rounded-4">
+            <div class="card col-md-6 col-lg-6 shadow-lg">
+                <div class="p-4">
                     <div class="card-body">
                         <h4 class="card-title mb-4 text-center">Recuperar Contraseña</h4>
-                        <p class="text-center mb-4">Introduce tu correo electrónico para recibir instrucciones sobre cómo recuperar tu contraseña.</p>
+                        <p class="txt text-center mb-4">Introduce tu correo electrónico para recibir instrucciones sobre cómo recuperar tu contraseña.</p>
                         <form action="../../Auth/forgotpassword.php" method="POST">
                             <div class="mb-3">
-                                <label for="emailInput" class="form-label">Correo Electrónico</label>
+                                <label for="emailInput" class="txt form-label">Correo Electrónico</label>
                                 <input type="email" class="form-control" id="emailInput" name="email" required placeholder="Tu correo electrónico">
                             </div>
                             <div class="text-center">
                                 <button type="submit" class="btn btn-primary w-100">Enviar</button>
                             </div>
                         </form>
-                        <div class="text-center mt-3">
+                        <div class="backlogin text-center mt-3">
                             <a href="login.php">Volver al inicio de sesión</a>
                         </div>
                     </div>
--- ProyectoPruebaPhp/Vistas/Auth/login.php
+++ ProyectoPruebaPhp/Vistas/Auth/login.php
@@ -12,7 +12,7 @@ require '../../Claves/claves.php';
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-  <link rel="stylesheet" href="../../Css/login/login.css">
+  <link rel="stylesheet" href="../../Css/auth/login.css">
   <link rel="stylesheet" href="../../Css/modal.css">
   <script src="https://www.google.com/recaptcha/api.js?hl=es" async defer></script>
 
@@ -24,27 +24,27 @@ require '../../Claves/claves.php';
     <div class="row justify-content-md-center mx-auto mt-5">
       <div class="col col-lg-12">
         <div class="col-xl-12 d-flex justify-content-center align-items-center mt-5">
-          <div class="col-sm-8 shadow-lg p-4 rounded-4">
-            <div class="card-body">
+          <div class="card col-sm-8 shadow-lg p-4 rounded-4">
+            <div class="body">
               <h4 class="card-title mb-4 text-center fst-italic fw-bold">Login - Libreria</h4>
               <div class="col-6 text-center mb-3">
                 <span class="mx-auto d-block"><a href="..\Home\index.php" class="link" ><i class="fas fa-undo-alt mx-1"></i>Volver al inicio</a></span>
               </div>
               <form action="..\..\Auth\login.php" method="POST">
                 <div class="row mb-4">
-                  <label for="horizontal-email-input" class="col-sm-3 col-form-label text-end fw-bold fst-italic">Email</label>
-                  <div class="col-sm-9">
+                  <label for="horizontal-email-input" class="txt col-sm-3 col-form-label text-end fw-bold fst-italic">Email</label>
+                  <div class="col-sm-6">
                     <input type="email" required name="email" class="form-control" id="horizontal-email-input" placeholder="Enter Your Email ID" />
                   </div>
                 </div>
                 <div class="row mb-4">
-                  <label for="horizontal-password-input" class="col-sm-3 col-form-label text-end fw-bold fst-italic">Password</label>
-                  <div class="col-sm-9">
+                  <label for="horizontal-password-input" class="txt col-sm-3 col-form-label text-end fw-bold fst-italic">Password</label>
+                  <div class="col-sm-6">
                     <input type="password" required name="password" class="form-control" id="horizontal-password-input" placeholder="Enter Your Password" />
                   </div>
                 </div>
                 <div class="row">
-                  <div class="col text-center">
+                  <div class="rounded-2 col text-center">
                     <div class="g-recaptcha d-inline-block" name="g-recaptcha-response" 
                     data-sitekey="<?php echo $claves['publicKey'] ?>"></div>
                   </div>
@@ -57,7 +57,7 @@ require '../../Claves/claves.php';
                   </div>
               </form>
               <div class="col-6 text-center mt-1">
-                <span class="mx-auto d-block"><a href="forgotpassword.php">Recuperar contraseña</a></span>
+                <span class="mx-auto d-block"><a href="forgotpassword.php" class="forgtopassword">Recuperar contraseña</a></span>
               </div>
             </div>
           </div>
